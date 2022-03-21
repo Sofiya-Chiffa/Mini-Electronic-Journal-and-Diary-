d84c02f5ad8dd102b9cd1c0412edb7a70a0005cc
Chiffa <sofiya_zhiltsova@mail.ru>
2022-03-21 23:32:37 +0300

    кнопки работают

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask('MyApp')
+
+
+@app.route('/')
+def teacher_or_student():
+    return render_template('teacher_or_student.html')
+
+
+@app.route('/teacher')
+def teacher():
+    return render_template('teacher.html')
+
+@app.route('/student')
+def student():
+    return render_template('student.html')
+
 
 app.run(port=8080, host='127.0.0.1', debug=True)
