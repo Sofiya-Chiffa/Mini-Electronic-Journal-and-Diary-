5b191524a0efa132fc9514094c7cb199577b985e
Chiffa <sofiya_zhiltsova@mail.ru>
2022-03-27 20:44:49 +0300

    кнопки работают

--- main.py
+++ main.py
@@ -8,13 +8,20 @@ def teacher_or_student():
     return render_template('teacher_or_student.html')
 
 
-@app.route('/teacher')
+@app.route('/teacher', methods=['POST', 'GET'])
 def teacher():
-    return render_template('teacher.html')
+    if request.method == 'GET':
+        return render_template('teacher.html')
+    elif request.method == 'POST':
+        return '1'
 
-@app.route('/student')
+
+@app.route('/student', methods=['POST', 'GET'])
 def student():
-    return render_template('student.html')
+    if request.method == 'GET':
+        return render_template('student.html')
+    elif request.method == 'POST':
+        return '1'
 
 
 app.run(port=8080, host='127.0.0.1', debug=True)
