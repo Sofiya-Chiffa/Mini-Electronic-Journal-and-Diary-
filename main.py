31fc197309c5fcbc3774e096e22f8889e6c7c164
Chiffa <sofiya_zhiltsova@mail.ru>
2022-04-05 00:01:51 +0300

    расписание студент

--- main.py
+++ main.py
@@ -9,19 +9,44 @@ def teacher_or_student():
 
 
 @app.route('/teacher', methods=['POST', 'GET'])
-def teacher():
+def teacher_enter():
     if request.method == 'GET':
-        return render_template('teacher.html')
+        return render_template('teacher_enter.html')
     elif request.method == 'POST':
         return '1'
 
 
 @app.route('/student', methods=['POST', 'GET'])
-def student():
+def student_enter():
     if request.method == 'GET':
-        return render_template('student.html')
+        return render_template('student_enter.html')
     elif request.method == 'POST':
         return '1'
+
+
+@app.route('/student/diary')
+def student_diary():
+    days = [[['Математика', '...'], ['Русский язык', '..'], ['Окружающий мир', ''], ['Литература', ''], ['', '']],
+            [['ИЗО', '.'], ['Математика', '...'], ['Русский язык', ''], ['Физ-ра', ''], ['Английский язык', '.']],
+            [['История', '.'], ['Информатика', ''], ['Русский язык', '.'], ['Технология', '..'], ['', '']],
+            [['География', '..'], ['Физ-ра', '.'], ['Математика', ''], ['Русский язык', '.'], ['', '']],
+            [['Окружающий мир', '..'], ['Математика', '....'], ['Музыка', '..'], ['Литература', '..'], ['', '']]]
+    return render_template('student_diary.html', days=days)
+
+
+@app.route('/student/schedule')
+def student_schedule():
+    days = [['Математика', 'Русский язык', 'Окружающий мир', 'Литература', ''],
+            ['ИЗО', 'Математика', 'Русский язык', 'Физ-ра', 'Английский язык'],
+            ['История', 'Информатика', 'Русский язык', 'Технология', ''],
+            ['География', 'Физ-ра', 'Математика', 'Русский язык', ''],
+            ['Окружающий мир', 'Математика', 'Музыка', 'Литература', '']]
+    return render_template('student_schedule.html', days=days)
+
+
+@app.route('/student/grade')
+def student_grade():
+    return render_template('student_grade.html')
 
 
 app.run(port=8080, host='127.0.0.1', debug=True)
