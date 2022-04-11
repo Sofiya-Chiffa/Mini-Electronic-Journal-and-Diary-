0ed94a0ac959bd0c07011267c67d7dbf0dc3d4c1
Chiffa <sofiya_zhiltsova@mail.ru>
2022-04-11 23:27:16 +0300

    добавлены оценки

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect
 from data import db_session
+from json import loads, dumps
+import datetime as dt
+from data.classes import Classes
+from data.homeworks import Homeworks
+from data.students import Students
+from data.teachers import Teachers
 
 app = Flask('MyApp')
 
@@ -23,10 +28,14 @@ def teacher_enter():
 
 @app.route('/student', methods=['POST', 'GET'])
 def student_enter():
+    global person_id
     if request.method == 'GET':
         return render_template('student_enter.html')
     elif request.method == 'POST':
-        return '1'
+        for user in session.query(Students).filter(Students.name.like(f'%{request.form["login"]}%')):
+            return redirect('/student/diary')
+        else:
+            return redirect('/student')
 
 
 @app.route('/student/diary')
@@ -45,18 +54,32 @@ def student_diary():
 
 @app.route('/student/schedule')
 def student_schedule():
-    days = {'Понедельник': ['Математика', 'Русский язык', 'Окружающий мир', 'Литература', ''],
+    # for user in session.query(Students).filter(Students.name.like('%Вася%')):
+    #    d_us = user.class_id.schedule
+    #    print(loads(d_us))
+    days = {'Понедельник': ['Математика', 'Русский язык', 'Окружающий мир', 'Литература', '-'],
             'Вторник': ['ИЗО', 'Математика', 'Русский язык', 'Физ-ра', 'Английский язык'],
-            'Среда': ['История', 'Информатика', 'Русский язык', 'Технология', ''],
-            'Четверг': ['География', 'Физ-ра', 'Математика', 'Русский язык', ''],
-            'Пятница': ['Окружающий мир', 'Математика', 'Музыка', 'Литература', '']
+            'Среда': ['История', 'Информатика', 'Русский язык', 'Технология', '-'],
+            'Четверг': ['География', 'Физ-ра', 'Математика', 'Русский язык', '-'],
+            'Пятница': ['Окружающий мир', 'Математика', 'Музыка', 'Литература', '-']
             }
     return render_template('student_schedule.html', days=days)
 
 
 @app.route('/student/grade')
 def student_grade():
-    return render_template('student_grade.html')
+    grades = {'subg1': [5, 4, 3, 5], 'subg2': [5, 4, 5, 5, 5, 5],
+              'subg3': [5, 4, 4, 4, 5]
+              }
+    grd_sum = {'subg1': round(sum(grades['subg1']) / len(grades['subg1']), 2),
+               'subg2': round(sum(grades['subg2']) / len(grades['subg2']), 2),
+               'subg3': round(sum(grades['subg3']) / len(grades['subg3']), 2)}
+    return render_template('student_grade.html', grades=grades, grd_sum=grd_sum)
+
+
+@app.route('/student/profile')
+def student_profile():
+    return render_template('student_profile.html')
 
 
 app.run(port=8080, host='127.0.0.1', debug=True)
