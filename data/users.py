6e58905c5118c6b1bec0da1a8901aa4ded3a8a8b
Vlad <vsakayafignavlad@gmail.com>
2022-04-12 20:22:14 +0300

    123

--- data/users.py
+++ data/users.py
@@ -1,17 +1,20 @@
 import sqlalchemy
+from sqlalchemy import null
+from flask_login import UserMixin
 from .db_session import ORMBase
 import sqlalchemy.orm as orm
 
 
-class Teachers(ORMBase):
-    __tablename__ = 'teachers'
+class Users(ORMBase, UserMixin):
+    __tablename__ = 'users'
 
-    t_id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, unique=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True, unique=True)
     name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     otchestvo = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     login = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    role = sqlalchemy.Column(sqlalchemy.String, nullable=True)
     class_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('classes.cl_id'), nullable=True)
 
-    cls = orm.relation('Classes', backref='teachers')
+    # cls = orm.relation('Classes', backref='students')
