6c579157a46291f10def299b79be297bb304f0a4
Chiffa <sofiya_zhiltsova@mail.ru>
2022-04-16 14:39:00 +0300

    heroku

--- main.py
+++ main.py
@@ -162,6 +162,4 @@ def student_profile():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
-    #  app.run(port=8080, host='127.0.0.1')
+    app.run(port=8080, host='127.0.0.1')
